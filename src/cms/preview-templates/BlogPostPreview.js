92b80e3b96bf99c4e606f90f7c881f05ab94928f
Test <karim_om@me.com>
2018-09-09 12:49:37 +0200

    Netlify CMS fix 2

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { BlogPostTemplate } from '../blog-post-preview/blog-post'
+import { BlogPostTemplate } from '../../templates/blog-post'
 
 const BlogPostPreview = ({ entry, widgetFor }) => (
   <BlogPostTemplate
