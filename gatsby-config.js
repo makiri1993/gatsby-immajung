c7e4b319e110ad98f009f2f9ae923d2b0af53c99
makiri <kireew.martin@icloud.com>
2019-01-15 20:49:12 +0100

    init coming soon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,69 +1,36 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    siteUrl: `https://www.immajung.com`,
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-125435855-1",
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-sitemap`
-    },
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: 'https://www.immajung.com',
-        sitemap: 'https://www.immajung.com/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/img`,
-        name: 'images',
-      },
-    },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [],
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-emotion`,
-      options: {
-        // Accepts all options defined by `babel-plugin-emotion` plugin.
-      },
-    },
+    'gatsby-plugin-emotion',
     'gatsby-plugin-typescript',
-    'gatsby-plugin-netlify' // make sure to keep it last in the array
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
   ],
 }
---
+++ src/definitions.ts
@@ -0,0 +1,21 @@
+export const Space = {
+  xsmall: '0.25rem',
+  small: '0.5rem',
+  medium: '1rem',
+  large: '1.5rem',
+  xlarge: '2rem',
+}
+
+export const Border = {
+  thin: '2px solid #70798c',
+}
+export const Shadow = {
+  light:
+    '4px 4px 40px 10px rgba(255, 64, 0, 0.2), 0 6px 20px 0 rgba(255, 64, 0, 0.19)',
+}
+
+export const Color = {
+  background: '#DAAD86',
+  font: 'black',
+  standard: 'rgba(255, 64, 0, 0.6)',
+}
